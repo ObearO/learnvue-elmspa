7048db3618432fd0f92da235af74a391b6590561
ObearO <Grant241818@outlook.com>
2017-07-05 23:58:37 +0800

    Complete ratings

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -26,11 +26,28 @@
       </div>
       <split></split>
       <ratingselect :select-type="selectType" :only-content="onlyContent" :ratings="ratings"></ratingselect>
-      <div class="ratings-wrapper">
+      <div class="rating-wrapper">
         <ul v-show="ratings.length">
-          <li class="rating-content" v-for="rating in ratings"></li>
+          <li class="rating-item" v-for="rating in ratings" v-show="needShow(rating.text,rating.rateType)">
+            <div class="avatar">
+              <img :src="rating.avatar" alt="" width="28" height="28">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+            </div>
+          </li>
         </ul>
-        <div class="no-ratings" v-show="!ratings.length"></div>
+        <div class="no-ratings" v-show="!ratings.length">暂无评论</div>
       </div>
     </div>
   </div>
@@ -41,6 +58,7 @@
   import BScroll from 'better-scroll';
 	import split from 'components/split/split';
 	import ratingselect from 'components/ratingselect/ratingselect';
+  import {formatDate} from 'common/js/date';
 
 	const ALL = 2;
   const ERR_OK = 0;
@@ -62,14 +80,46 @@
 				response = response.body;
 				if (response.errno === ERR_OK) {
 					this.ratings = response.data;
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$els.ratings, {
+              click: true
+            });
+          });
         }
 			});
-			this.$nextTick(() => {
-        this.scroll = new BScroll(this.$els.ratings, {
-          click: true
-        });
-      });
 		},
+    events: {
+      'ratingtype.select'(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      }
+    },
+    methods: {
+      needShow(text, rateType) {
+        if (!text && this.onlyContent) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return this.selectType === rateType;
+        }
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
+      }
+    },
 		components: {
 			star,
 			split,
@@ -79,6 +129,8 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .ratings
     position absolute
     top 174px
@@ -148,5 +200,68 @@
               line-height 18px
               margin-left 12px
               font-size 12px
+              color rgb(147, 153, 159)
+      .rating-wrapper
+        padding 0 18px
+        .rating-item
+          display flex
+          padding 18px 0
+          border-1px(rgba(7, 17, 27, 0.1))
+          .avatar
+            flex 0 0 28px
+            width 28px
+            margin-right 12px
+            img
+              border-radius 50%
+          .content
+            position relative
+            flex 1
+            .name
+              margin-bottom 4px
+              line-height 12px
+              font-size 10px
+              color rgb(7, 17, 27)
+            .star-wrapper
+              margin-bottom 6px
+              font-size 0
+              .delivery
+                display inline-block
+                vertical-align top
+                line-height 12px
+                font-size 10px
+                font-weight 200
+                color rgb(147, 153, 159)
+              .star
+                display inline-block
+                vertical-align top
+                margin-right 6px
+            .text
+              margin-bottom 8px
+              font-size 12px
+              line-height 18px
+              color rgb(7, 17, 27)
+            .recommend
+              font-size 0
+              line-height 16px
+              .icon-thumb_up, .item
+                display inline-block
+                margin 0 8px 4px 0
+              .icon-thumb_up
+                font-size 12px
+                color rgb(0, 160 ,220)
+              .item
+                padding 0 6px
+                border 1px solid rgba(7, 17, 27, 0.1)
+                border-radius 2px
+                font-size 9px
+                color rgb(147, 153, 159)
+                background #fff
+            .time
+              position absolute
+              top 0
+              right 0
+              line-height 12px
+              font-size 10px
+              font-weight 200
               color rgb(147, 153, 159)
 </style>
