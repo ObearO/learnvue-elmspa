5eed110b685a9c62fc32deba3c16fefadc65173d
ObearO <Grant241818@outlook.com>
2017-07-05 18:10:49 +0800

    Modify ratings.vue

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ratings">
+  <div class="ratings" v-el:ratings>
     <div class="ratings-content">
       <div class="overview">
         <div class="over-left">
@@ -24,22 +24,57 @@
           </div>
         </div>
       </div>
+      <split></split>
+      <ratingselect :select-type="selectType" :only-content="onlyContent" :ratings="ratings"></ratingselect>
+      <div class="ratings-wrapper">
+        <ul v-show="ratings.length">
+          <li class="rating-content" v-for="rating in ratings"></li>
+        </ul>
+        <div class="no-ratings" v-show="!ratings.length"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import star from 'components/star/star';
+	import star from 'components/star/star';
+  import BScroll from 'better-scroll';
+	import split from 'components/split/split';
+	import ratingselect from 'components/ratingselect/ratingselect';
 
+	const ALL = 2;
+  const ERR_OK = 0;
 	export default {
 		props: {
 			seller: {
 				type: Object
 			}
 		},
-    components: {
-		  star
-    }
+		data() {
+			return {
+				ratings: [],
+				selectType: ALL,
+				onlyContent: true
+			};
+		},
+		created() {
+			this.$http.get('/api/ratings').then((response) => {
+				response = response.body;
+				if (response.errno === ERR_OK) {
+					this.ratings = response.data;
+        }
+			});
+			this.$nextTick(() => {
+        this.scroll = new BScroll(this.$els.ratings, {
+          click: true
+        });
+      });
+		},
+		components: {
+			star,
+			split,
+			ratingselect
+		}
 	};
 </script>
 
@@ -52,13 +87,13 @@
     width 100%
     overflow hidden
     .ratings-content
-      padding 18px 0
       .overview
+        padding 18px 0
         display flex
         .over-left
-          flex 0 0 137px
+          flex 0 0 37%
           padding 6px 0
-          width 137px
+          width 37%
           border-right 1px solid rgba(7, 17, 27, 0.1)
           text-align center
           .score
@@ -78,35 +113,39 @@
         .over-right
           flex 1
           padding 6px 0 6px 24px
+          @media only screen and (max-width: 320px)
+            padding-left 6px
           .score-wrapper
             margin-bottom 8px
-            line-height 18px
             font-size 0
             .title
               display inline-block
               vertical-align top
+              line-height 18px
               font-size 12px
               color rgb(7, 17, 27)
             .star
               display inline-block
-              margin 0 12px
               vertical-align top
+              margin 0 12px
             .score
               display inline-block
               vertical-align top
+              line-height 18px
               font-size 12px
               color: rgb(255, 153, 0)
           .delivery-wrapper
-            line-height 18px
             font-size 0
             .title
               display inline-block
               vertical-align top
+              line-height 18px
               font-size 12px
               color rgb(7, 17, 27)
             .delivery
               display inline-block
               vertical-align top
+              line-height 18px
               margin-left 12px
               font-size 12px
               color rgb(147, 153, 159)
